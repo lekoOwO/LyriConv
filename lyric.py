f613b1c3e6cdbd512d9f20ae7ee8f418c357fefa
Rex Tseng <rexx0520@gmail.com>
2018-06-29 23:32:48 +0800

    現在 API 可以接受更多的雞掰歌詞

--- lyric.py
+++ lyric.py
@@ -8,14 +8,12 @@ def chs_to_cht(chs):
 
 offset = (0.0001,4)
 parse_lyric = lambda x:re.findall('(?:\[(.+?)\])(.*|\n)', x)
-tag_to_sec = lambda x:sum([float(list(reversed(x.split(":")))[i]) * (60 ** i) for i in range(len(list(reversed(x.split(":")))))]) if x[0].isdigit() else x
-plyric_to_ptlyric = lambda x:[(x[i+1 if i!=len(x)-1 else i][0] + (-offset[0] if i!=len(x)-1 else offset[0]) if type(x[i][0]) == float else x[i][0], x[i][1]) for i in range(len(x))]
+tag_to_sec = lambda x:sum([float(list(reversed(x.split(":")))[i]) * (60 ** i) for i in range(len(list(reversed(x.split(":")))))]) if type(x) == str and x[0].isdigit() else x
+get_next_index = lambda x,y:y.index(list(filter(lambda k:False if not type(k[0]) == float else -10*offset[0] <= tag_to_sec(k[0])-x[0] <= 10*offset[0], y))[0])+1
+plyric_to_ptlyric = lambda x,y:[(i[0] if type(i[0]) != float else y[-1][0]+offset[0] if get_next_index(i,y) >= len(y) else y[get_next_index(i,y)][0] - offset[0], i[1]) for i in x]
 plyric_to_lyric = lambda x:reduce(lambda b,c:b+'\n'+c, map(lambda a:(('[{:.0f}:{:.%sf}]{}' % offset[1]) if type(a[0]) == float else '[{}]{}').format(*(divmod(a[0], 60) if type(a[0]) == float else [a[0]]), a[1]), x))
 
 def migrate(org, translated):
-    migrated = org + '\n' + plyric_to_lyric(plyric_to_ptlyric([(tag_to_sec(i[0]), i[1]) for i in parse_lyric(translated)]))
-    pmigrated = [[tag_to_sec(i[0]), i[1]] for i in parse_lyric(migrated)]
-    for i in range(len(pmigrated)-1):
-        if pmigrated[i][1] == '' and type(pmigrated[i+1][0]) == float and pmigrated[i+1][1]!='' and i>0 and type(pmigrated[i-1][0]) == float:
-            pmigrated[i+1][0] =  pmigrated[i][0] - offset[0]
-    return '\n'.join(['[{}]{}'.format(x[0], x[1]) for x in sorted(parse_lyric(plyric_to_lyric(pmigrated)), key=lambda x:0 if not x[0][0].isdigit() else tag_to_sec(x[0]))])
+    migrated = org + '\n' + plyric_to_lyric(plyric_to_ptlyric([(tag_to_sec(i[0]), i[1]) for i in parse_lyric(translated)], [(tag_to_sec(i[0]), i[1]) for i in parse_lyric(org)]))
+    pmigrated = sorted([[tag_to_sec(i[0]), i[1]] for i in parse_lyric(migrated)], key=lambda x:x[0] if type(x[0]) == float else 0)
+    return plyric_to_lyric(sorted(parse_lyric(plyric_to_lyric(pmigrated)), key=lambda x:0 if not x[0][0].isdigit() else tag_to_sec(x[0])))
